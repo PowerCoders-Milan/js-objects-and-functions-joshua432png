e5db3e9b6d12a2929d0b5e08233a6f02509dc1ec
Joshua Udeh <udehasilo@gmail.com>
2021-02-26 13:07:22 +0100

    exercises

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,3 +1,13 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+function user() {
+    var noun = prompt("Can you please input a noun");
+    var verb = prompt("please input an verb");
+    var adverb = prompt("Please input an adverb");
+    var adjective = prompt("Please input an adjective");
+    var user = ("I love " + verb + " football " + " and I live in " + noun + " . " + " she spoke " + adverb + " she wore a " + adjective + "dress");
+    window.alert(user)
+}
+
+user();
--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,3 +1,23 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+
+
+var children = prompt("Please number of children");
+var partner = prompt("please your partner's name");
+var city = prompt("Please your geogragphic location");
+var job = prompt("Please your job title");
+
+function tellFortune(children, partner, location, job) {
+    this.children = children
+    this.partner = partner
+    this.location = location
+    this.job = job
+    this.fortune = function() {
+        window.alert("You are going to be married to " + partner + " live in " + location)
+    }
+}
+var joshua = new tellFortune(children, partner, city, job);
+joshua.fortune()
+var hassan = new tellFortune(4, "sarah", "switzerland", "developer");
+hassan.fortune()
