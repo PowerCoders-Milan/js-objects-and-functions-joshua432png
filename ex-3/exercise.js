f9266b51457fb9ef61f8b38c79f7f8c742cf933e
Joshua Udeh <udehasilo@gmail.com>
2021-02-26 17:10:27 +0100

    exercise 3

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,3 +1,16 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+var choices = [colors, cities];
+var colors = ["red", "pink", "purple", "brown", "blue"];
+window.alert("My #1 choice is " + colors[2]);
+var cities = ["Milan", "venice", "London", "Newyork"];
+window.alert("My #1 choice is " + cities[1]);
+
+const favourites = {
+    color: "Blue",
+    city: "Milan"
+}
+window.alert("My favourite color is " + favourites.color);
+window.alert("My favourite city is " + favourites.city);
+
