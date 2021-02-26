73322998b87146a8701b8011c3a4969f7c5db49f
Joshua Udeh <udehasilo@gmail.com>
2021-02-26 17:41:30 +0100

    exercise 4

--- ex-4/exercise.js
+++ ex-4/exercise.js
@@ -1,3 +1,12 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+function ageCalculator() {
+    birthYear = prompt("Please put your Birthday year");
+    actualYear = new Date();
+    age = actualYear.getFullYear()-birthYear
+    return "Your age is " + age
+}
+
+window.alert(ageCalculator())
+window.alert(ageCalculator())
