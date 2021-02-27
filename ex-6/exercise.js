2bf00bfac7d0f9c9a3a3fa36c840178a7e01f651
Joshua Udeh <udehasilo@gmail.com>
2021-02-27 15:01:38 +0100

    exercise 6

--- ex-6/exercise.js
+++ ex-6/exercise.js
@@ -1,3 +1,14 @@
 // write your JS here
 // remember to always test in the browser
 // to see if everything works correctly
+function greaterNum (num1, num2) {
+    if (num1 > num2) {
+        console.log("The greater number of " + num1 + " and " + num2 + " is " + num1);
+    } else if (num2 > num1) {
+        console.log("The greater number of " + num1 + " and " + num2 + " is " + num2);
+    } else {
+        console.log("The numbers are the same");
+    }
+}
+greaterNum(40, 30);
+greaterNum(50,20);
